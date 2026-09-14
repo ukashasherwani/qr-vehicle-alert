ca2a3423961be4b7390c5cc2a4055d893ff519b7
Nabeel Mughal <nabeelmughal295997@gmail.com>
2026-09-14 11:21:18 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const io = new Server(httpServer, {
     methods: ['GET', 'POST'],
   },
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 validateEnvironment();
 
